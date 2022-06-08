40f21657a5392d1a1151d1a2930deec43ab6059f
Pagwin <git@pagwin.xyz>
2022-06-08 01:16:34 -0400

    I think it may work but it's also possible the linker script isn't correct

--- gdt.s
+++ gdt.s
@@ -2,4 +2,42 @@
 //make sure the linker puts this with the code
 .section text
 setupGdt:
+    lgdt [GDT_REG]
+    mov %eax, %cr0
+    or  %eax, 0x1
+    mov %cr0, %eax
+    ret
+.section gdt
+GDT_START:
+GDT_NULL:
+    .quad 0x0
+//copied from https://github.com/falconnine9/Talon/blob/main/boot/gdt.asm
+GDT_SEG_KCODE:
+    .hword 0xFFFF
+    .hword 0
+    .word 0
+    .word 0x9A
+    .word 0xCF
+    .word 0
 
+GDT_SEG_KDATA:
+    .hword 0xFFFF
+    .hword 0
+    .word 0
+    .word 0x92
+    .word 0xCF
+    .word 0
+
+GDT_SEG_TSS:
+    .hword 0x68
+    .hword 0
+    .word 0
+    .word 0x81
+    .word 0x40
+    .word 0
+
+GDT_END:
+
+GDT_REG:
+    .hword GDT_END - GDT_START - 1
+    .int GDT_START
